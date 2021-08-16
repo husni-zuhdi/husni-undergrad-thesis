344df0b6d39254ad8a7a66c56a8a74e34734688a
Hazunanafaru <hazunanafaru@gmail.com>
2021-08-16 01:30:27 +0000

    Update run.sh to run 0.1 mod ratio 0.3 neutron_multiplier simulations

--- jupyter-notebooks/1-calculate-tbr/run.sh
+++ jupyter-notebooks/1-calculate-tbr/run.sh
@@ -43,11 +43,34 @@
 #done
 
 # Run 0.1 mod_ratio 0 neutron_multiplier simulation with fixed code
+#for i in 0.1 0.3 0.5 0.7 0.9
+#do
+#  cd ~/iter-tritium-breeding-xgboost/jupyter-notebooks/1-calculate-tbr
+#  mkdir  "calculate_tbr/$i-0.1-0-100-1000"
+#  cp calculate_tbr.py "calculate_tbr/$i-0.1-0-100-1000/calculate_tbr.py"
+#  cd "calculate_tbr/$i-0.1-0-100-1000"
+#  python calculate_tbr.py --enrich_Li $i --mod_ratio 0.1 --neutron_multi 0 --batch 100  --part_batch 1000
+#done
+
+# Run 0.1 mod_ratio 0.1 neutron_multiplier simulation with fixed code
+#for i in 0.1 0.3 0.5
+for i in 0.7 0.9
+do
+  cd ~/iter-tritium-breeding-xgboost/jupyter-notebooks/1-calculate-tbr
+  mkdir  "calculate_tbr/$i-0.1-0.1-100-1000"
+  cp calculate_tbr.py "calculate_tbr/$i-0.1-0.1-100-1000/calculate_tbr.py"
+  cd "calculate_tbr/$i-0.1-0.1-100-1000"
+  python calculate_tbr.py --enrich_Li $i --mod_ratio 0.1 \
+  --neutron_multi 0.1 --batch 100 --part_batch 1000
+done
+
+# Run 0.1 mod_ratio 0.3 neutron_multiplier simulation with fixed code
 for i in 0.1 0.3 0.5 0.7 0.9
 do
   cd ~/iter-tritium-breeding-xgboost/jupyter-notebooks/1-calculate-tbr
-  mkdir  "calculate_tbr/$i-0.1-0-100-1000"
-  cp calculate_tbr.py "calculate_tbr/$i-0.1-0-100-1000/calculate_tbr.py"
-  cd "calculate_tbr/$i-0.1-0-100-1000"
-  python calculate_tbr.py --enrich_Li $i --mod_ratio 0.1 --neutron_multi 0 --batch 100  --part_batch 1000
+  mkdir  "calculate_tbr/$i-0.1-0.3-100-1000"
+  cp calculate_tbr.py "calculate_tbr/$i-0.1-0.3-100-1000/calculate_tbr.py"
+  cd "calculate_tbr/$i-0.1-0.3-100-1000"
+  python calculate_tbr.py --enrich_Li $i --mod_ratio 0.1 \
+  --neutron_multi 0.3 --batch 100 --part_batch 1000
 done
