28901642b08214919f76d03f882b026304f8be4c
Hazunanafaru <hazunanafaru@gmail.com>
2021-08-16 08:07:59 +0000

    Add upload to bucket line

--- jupyter-notebooks/1-calculate-tbr/run.sh
+++ jupyter-notebooks/1-calculate-tbr/run.sh
@@ -71,6 +71,11 @@ do
   mkdir  "calculate_tbr/$i-0.1-0.3-100-1000"
   cp calculate_tbr.py "calculate_tbr/$i-0.1-0.3-100-1000/calculate_tbr.py"
   cd "calculate_tbr/$i-0.1-0.3-100-1000"
+  echo "Start simulating $i Li-6 enrichment + 0.1 Mod Ratio + 0.3 Neutron Multiplier Ratio"
   python calculate_tbr.py --enrich_Li $i --mod_ratio 0.1 \
   --neutron_multi 0.3 --batch 100 --part_batch 1000
+  
+  # If you want to save your simulation result in a bucket (I use google cloud storage her)
+  # make sure to change the name of bucket. If not, comment the code bellow
+  gsutil cp results.json gs://iter-tritium-breeding-xgboost-bucket/results/$i-0.1-0.3-100-1000.json
 done
