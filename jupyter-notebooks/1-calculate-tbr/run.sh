3e690e32a4b744900ae24a8ea5bd58cae8d370c1
Hazunanafaru <hazunanafaru@gmail.com>
2021-09-06 15:50:37 +0000

    Update run.sh file

--- jupyter-notebooks/1-calculate-tbr/run.sh
+++ jupyter-notebooks/1-calculate-tbr/run.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 # Change mod and nm number
-export mod=0.2
-export nm=0.5
-for i in 0.5 0.7 0.9
+# export mod=0
+export mod=0.7
+for nm in 0.3 0.5
 do
-  cd ~/iter-tritium-breeding-xgboost/jupyter-notebooks/1-calculate-tbr
-  mkdir  "calculate_tbr/$i-$mod-$nm-100-1000"
-  cp calculate_tbr.py "calculate_tbr/$i-$mod-$nm-100-1000/calculate_tbr.py"
-  cd "calculate_tbr/$i-$mod-$nm-100-1000"
-  echo "Start simulating $i Li-6 enrichment + $mod Mod Ratio + $nm Neutron Multiplier Ratio"
-  python calculate_tbr.py --enrich_Li $i --mod_ratio $mod \
-  --neutron_multi $nm --batch 100 --part_batch 1000
-  
-  # If you want to save your simulation result in a bucket (I use google cloud storage her)
-  # make sure to change the name of bucket. If not, comment the code bellow
-  gsutil cp results.json gs://iter-tritium-breeding-xgboost-bucket/v2/results/$i-$mod-$nm-100-1000.json
-  gsutil cp statepoint.100.h5 gs://iter-tritium-breeding-xgboost-bucket/v2/statepoint/$i-$mod-$nm-100-1000.h5
-  gsutil cp tallies.out gs://iter-tritium-breeding-xgboost-bucket/v2/tallies/$i-$mod-$nm-100-1000.out
-  gsutil cp n-Xt_on_3D_mesh.vtk gs://iter-tritium-breeding-xgboost-bucket/v2/3D-mesh/$i-$mod-$nm-100-1000.vtk
-  gsutil cp n-Xt_on_2D_mesh_xy.png gs://iter-tritium-breeding-xgboost-bucket/v2/2D-mesh/xy/$i-$mod-$nm-100-1000.png
-  gsutil cp n-Xt_on_2D_mesh_xz.png gs://iter-tritium-breeding-xgboost-bucket/v2/2D-mesh/xz/$i-$mod-$nm-100-1000.png
-  gsutil cp n-Xt_on_2D_mesh_yz.png gs://iter-tritium-breeding-xgboost-bucket/v2/2D-mesh/yz/$i-$mod-$nm-100-1000.png
+  for i in 0.1 0.3 0.5 0.7 0.9
+  do
+    cd ~/iter-tritium-breeding-xgboost/jupyter-notebooks/1-calculate-tbr
+    mkdir  "calculate_tbr/$i-$mod-$nm-100-1000"
+    cp calculate_tbr.py "calculate_tbr/$i-$mod-$nm-100-1000/calculate_tbr.py"
+    cd "calculate_tbr/$i-$mod-$nm-100-1000"
+    echo "==================================================================================="
+    echo "Start simulating $i Li-6 enrichment + $mod Mod Ratio + $nm Neutron Multiplier Ratio"
+    echo "==================================================================================="
+    python calculate_tbr.py --enrich_Li $i --mod_ratio $mod \
+    --neutron_multi $nm --batch 100 --part_batch 1000
+
+    # If you want to save your simulation result in a bucket (I use google cloud storage her)
+    # make sure to change the name of bucket. If not, comment the code bellow
+    gsutil cp results.json gs://iter-tritium-breeding-xgboost-bucket/v2/results/$i-$mod-$nm-100-1000.json
+    gsutil cp statepoint.100.h5 gs://iter-tritium-breeding-xgboost-bucket/v2/statepoint/$i-$mod-$nm-100-1000.h5
+    gsutil cp tallies.out gs://iter-tritium-breeding-xgboost-bucket/v2/tallies/$i-$mod-$nm-100-1000.out
+    gsutil cp n-Xt_on_3D_mesh.vtk gs://iter-tritium-breeding-xgboost-bucket/v2/3D-mesh/$i-$mod-$nm-100-1000.vtk
+    gsutil cp n-Xt_on_2D_mesh_xy.png gs://iter-tritium-breeding-xgboost-bucket/v2/2D-mesh/xy/$i-$mod-$nm-100-1000.png
+    gsutil cp n-Xt_on_2D_mesh_xz.png gs://iter-tritium-breeding-xgboost-bucket/v2/2D-mesh/xz/$i-$mod-$nm-100-1000.png
+    gsutil cp n-Xt_on_2D_mesh_yz.png gs://iter-tritium-breeding-xgboost-bucket/v2/2D-mesh/yz/$i-$mod-$nm-100-1000.png
+  done
 done
-# Next mod=0.3 nm=0.1
+# Next mod=0.7 nm=0.7 and nm=0.9
