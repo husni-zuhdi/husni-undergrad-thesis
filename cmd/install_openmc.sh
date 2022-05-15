2a2cb45a056338550bea640de7d683175b04fc06
Husni Zuhdi <husni.zuhdi@tokopedia.com>
2022-05-15 08:35:25 +0700

    * Update install_openmc.sh file. Need to test in vm to verify

--- cmd/install_openmc.sh
+++ cmd/install_openmc.sh
@@ -49,12 +49,9 @@ export PATH="~/.local/bin:$PATH"
 echo INSTALL MOAB
 echo ======================
 read -p "Press any key to resume ..."
-cd ~/
-mkdir ~/MOAB
+mkdir -p ~/MOAB/build
 cd ~/MOAB
 git clone -b Version5.1.0 https://bitbucket.org/fathomteam/moab/
-cd ~/MOAB
-mkdir ~/MOAB/build
 cd ~/MOAB/build
 cmake ../moab -DENABLE_HDF5=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=~/MOAB -DENABLE_PYMOAB=ON
 export PYTHONPATH=~/MOAB/lib/python3.8/site-packages/:$PYTHONPATH
@@ -67,43 +64,34 @@ export LD_LIBRARY_PATH=~/MOAB/lib:$LD_LIBRARY_PATH
 echo INSTALL DAGMC
 echo ======================
 read -p "Press any key to resume ..."
-cd ~/
-mkdir ~/DAGMC
+mkdir -p ~/DAGMC/build
 cd ~/DAGMC
 git clone -b develop https://github.com/svalinn/dagmc
-cd ~/DAGMC
-mkdir ~/DAGMC/build
 cd ~/DAGMC/build
 cmake ../dagmc -DBUILD_TALLY=ON -DCMAKE_INSTALL_PREFIX=/home/$USER/DAGMC -DMOAB_DIR=/home/$USER/MOAB -DBUILD_STATIC_LIBS=OFF
 make install
 rm -rf ~/DAGMC/dagmc
 env LD_LIBRARY_PATH=~/DAGMC/lib:$LD_LIBRARY_PATH
 
-
-# %env FC=mpif90
-# %env CC=mpicc
-
 echo INSTALL OPENMC
 echo ======================
 read -p "Press any key to resume ..."
 cd ~/
 git clone https://github.com/openmc-dev/openmc.git
-cd ~/openmc
-mkdir ~/openmc/build
+mkdir -p ~/openmc/build
 cd ~/openmc/build
 cmake -Ddagmc=ON -DCMAKE_PREFIX_PATH=~/DAGMC ..
 make
-sudo make install
+# sudo make install
+make install
 export PATH="$PATH:~/openmc/build/bin"
 sudo cp ~/openmc/build/bin/openmc /usr/local/bin
-
 cd ~/openmc
 pip3 install .
 
 echo DOWNLOAD ENDF/B-VIII.0 NUCLEAR DATA
 echo ======================
 read -p "Press any key to resume ..."
-cd ~/
 mkdir ~/ND
 wget -O ~/ND/endfb80.tar.xz https://anl.box.com/shared/static/uhbxlrx7hvxqw27psymfbhi7bx7s6u6a.xz
 cd ~/ND
@@ -118,9 +106,12 @@ echo "# OpenMC Nuclear Database ENDF VIII.o" >> ~/.bashrc
 echo "export OPENMC_CROSS_SECTIONS='/home/$USER/ND/endfb80_hdf5/cross_sections.xml'" >> ~/.bashrc
 source ~/.bashrc
 
-echo INSTALL JUPYTERLAB
+echo INSTALL AND LAUNCH JUPYTERLAB
 echo ======================
 read -p "Press any key to resume ..."
-sudo pip3 install jupyterlab
-sudo jupyter serverextension enable --py jupyterlab --sys-prefix
+# Should we use sudo?
+# sudo pip3 install jupyterlab
+# sudo jupyter serverextension enable --py jupyterlab --sys-prefix
+pip3 install jupyterlab
+jupyter serverextension enable --py jupyterlab --sys-prefix
 jupyter lab --ip 0.0.0.0 --port 8888 --no-browser
